5e51d5706a7ba60ed786e917a0ebe816e25ce854
Souravkajla <159424805+Souravkajla@users.noreply.github.com>
2024-08-06 13:30:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,132 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// Connect to Mongo DB
+
+mongoose
+  .connect("mongodb+srv://skkajla1628:sourav03@cluster0.brq6qpi.mongodb.net/")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+
+// Making Schema
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+const productSchema = new mongoose.Schema({
+  productid: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+});
+
+// Creating models
+
+const User = mongoose.model("User", userSchema);
+const Product = mongoose.model("Product", productSchema);
+
+app.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const existUser = await User.findOne({ username, email });
+  if (existUser) {
+    return res.json({ message: "Username or email already exist" });
+  } else {
+    const user = new User({ username, email, password });
+    await user.save();
+  }
+  res.json({
+    message: "User Created Succsesfully",
+    username,
+  });
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const logon = await User.findOne({ email, password });
+  if (logon) {
+    res.json({
+      message: "Login Successfully",
+    });
+  } else {
+    res.json({
+      message: "Invalid email or password",
+    });
+  }
+});
+
+app.post("/addproduct", async (req, res) => {
+  const { productid, title, price } = req.body;
+
+  const existProduct = await Product.findOne({ productid });
+  if (existProduct) {
+    res.send({
+      message: "Product already added",
+    });
+  } else {
+    const product = new Product({ productid, title, price });
+    await product.save();
+    res.json({
+      message: "Product added successfully",
+      productid,
+    });
+  }
+});
+
+app.post("/purchaseproduct", async (req, res) => {
+  const { email, password, productid } = req.body;
+  const user = await User.findOne({ email, password });
+
+  if (!user) {
+    return res.json({
+      message: "User Not found",
+    });
+  }
+
+  const product = await Product.findOne({ productid });
+  if (product) {
+    return res.json({
+      message: "Purchased Successfully",
+      product,
+    });
+  } else {
+    return res.send({
+      message: "Wrong ProductId or Email",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
